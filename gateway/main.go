5dd8c1ecf5ad3e717b31241c23928418be5694d5
Parth Kapoor <parthkapoor.coder@gmail.com>
2025-05-06 13:33:55 +0530

    Gateway Service Init

--- gateway/main.go
+++ gateway/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+
+	_ "github.com/joho/godotenv/autoload"
+	common "github.com/parthkapoor-dev/common"
 )
 
-const (
-	httpAddr = ":3000"
+var (
+	httpAddr = common.EnvString("HTTP_ADDR", ":8080")
 )
 
 func main() {
